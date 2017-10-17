323d396ff18bde3ee709ace511640784fe97e4d5
guoshencheng <648772021@qq.com>
2017-10-17 23:01:01 +0800

    clear code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createApp } from 'ayano-react';
 
-//redux actions and reducers
 import reducers from './scripts/reducers';
 import * as actions from './scripts/actions';
-
-// NEW FEATURE: this is deprecated with option auto true and object type reducer
-console.warn(` ayano-REACT!!! NEW FEATURE: src/index.js line 12 deprecated with option auto true and object type reducer`);
 import constants from './scripts/constants.js';
 
-// api routers define
 import apis from './scripts/apis.js';
 import routers from './routers';
 
