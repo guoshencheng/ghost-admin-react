c58f1c8231985d5ebdd11b2728ebb026969e482f
guoshencheng <648772021@qq.com>
2017-10-21 17:14:48 +0800

    add admin layout sider

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connectApp } from 'ayano-react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-const FormItem = Form.Item;
+import { Layout, Menu, Icon } from 'antd';
+const { Sider, Content } = Layout;
+import './Admin.scss';
 
 class Admin extends React.Component {
   constructor(props, context) {
@@ -11,7 +12,34 @@ class Admin extends React.Component {
   render() {
     return (
       <div className="admin-container">
-
+        <Layout>
+          <Sider breakpoint="xs">
+            <div className="logo-icon custom-menu"></div>
+            <span className="navi-title custom-menu">功能</span>
+            <Menu>
+              <Menu.Item>
+                <Icon type="edit"></Icon>
+                <span className="nav-text">新建博文</span>
+              </Menu.Item>
+              <Menu.Item>
+                <Icon type="file-text"></Icon>
+                <span className="nav-text">博文列表</span>
+              </Menu.Item>
+              <Menu.Item>
+                <Icon type="team"></Icon>
+                <span className="nav-text">用户管理</span>
+              </Menu.Item>
+            </Menu>
+            <span className="navi-title custom-menu">设置</span>
+            <Menu>
+              <Menu.Item>
+                <Icon type="setting"></Icon>
+                <span className="nav-text">全局设置</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content>content</Content>
+        </Layout>
       </div>
     )
   }
