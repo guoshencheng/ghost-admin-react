ace22792d787dc62f052ccaf33bbec699cf5711e
guoshencheng <648772021@qq.com>
2018-01-09 23:01:07 +0800

    submit login

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -9,13 +9,20 @@ class Login extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
+
+  onSubmit() {
+    const { actions, form } = this.props;
+    const fields = form.getFieldsValue();
+    actions.auth.login(fields)
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="login-container">
         <Form className="login-form">
           <FormItem>
-            {getFieldDecorator('userName', {
+            {getFieldDecorator('username', {
               rules: [{ required: true, message: 'Please input your username!' }],
             })(
               <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
@@ -28,7 +35,7 @@ class Login extends React.Component {
               <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
             )}
           </FormItem>
-          <Button type="primary" htmlType="submit" className="login-form-button">
+          <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.onSubmit.bind(this)}>
               Log in
           </Button>
         </Form>
